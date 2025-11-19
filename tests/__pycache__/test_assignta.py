ad93a46a9190e4b533c1330c6563d912aff4bf11
iasolb <ianspraguesolberg@gmail.com>
2025-11-19 11:25:03 -0500

    what goin on,

---
+++ tests/__pycache__/test_assignta.py
@@ -0,0 +1,15 @@
+""" "
+Place to run tests for `assignta.py` using Pytest Unit tests.
+"""
+
+
+def test_objective():
+    pass
+
+
+def main():
+    test_objective()
+
+
+if __name__ == "__main__":
+    main()
